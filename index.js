99ab7350dcc1f6e8f0a64a612cd893ff3b3a9b54
fredshi90 <bonlafred90@gmail.com>
2024-12-11 01:07:12 +0100

    added new file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const userRouter = require('./routes/user.route');
 const morgan = require('morgan');
 
 require('dotenv').config();
@@ -10,20 +11,28 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.use(morgan('dev'));
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
-res.render('index', { message: 'Hello From Node.js' });
+  res.render('index', { message: 'Hello From Node.js' });
 });
+
+app.use('/users', userRouter);
+
 app.get('/contact', (req, res) => {
-res.render('index', { message: 'The Contact Page' });
+  res.render('index', { message: 'The Contact Page' });
 });
+
 app.get('/about', (req, res) => {
-res.render('index', { message: 'The About Page' });
+  res.render('index', { message: 'The About Page' });
 });
+
 app.get('*', (req, res) => {
-res.status(404).render('index', { message: 'Not Found' });
+  res.status(404).render('index', { message: 'Not Found' });
 });
+
 const PORT = 3000;
+
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
